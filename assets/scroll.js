8de3b46074ef726349f3c36bb0cc5a95b625e83c
Trey Hardin <treyhardindesign@gmail.com>
2021-10-06 10:21:11 -0400

    RTE Section

--- assets/scroll.js
+++ assets/scroll.js
@@ -6,7 +6,8 @@ if (!Shopify.designMode) {
         lerp: 0.3
     });
     scroll.on('scroll', (instance) => {
-        // console.log(instance.scroll.y);
+        
+        // Transparent Header Toggle
         let bodyElement = document.querySelector('body');
         let headerElement = document.querySelector('.header');
         if (bodyElement.classList.contains("banner-page")) {
@@ -18,5 +19,29 @@ if (!Shopify.designMode) {
                 headerElement.classList.add("transparent-header");
             }
         }
+
+        // Circle Badge Animation
+        let circleText = document.querySelector('.circle-text');
+        let computedStyles = window.getComputedStyle(circleText, null);
+        let transformStyle = computedStyles.getPropertyValue('transform');
+
+        var values = transformStyle.split('(')[1],
+            values = values.split(')')[0],
+            values = values.split(',');
+
+        var a = values[0]; // 0.866025
+        var b = values[1]; // 0.5
+        var c = values[2]; // -0.5
+        var d = values[3]; // 0.866025
+
+        var angle = Math.round(Math.asin(b) * (180/Math.PI));
+        
+        
+        angle = (angle + instance.scroll.y) / 10;
+        let transformAngle = "rotate(" + angle + "deg)";
+
+        circleText.style.transform = transformAngle;
+
+
     });
 }
